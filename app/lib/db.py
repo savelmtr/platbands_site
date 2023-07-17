9fe2c2740eca751cbc0669af09949d884d9c4288
savelmtr <56698459+savelmtr@users.noreply.github.com>
2023-07-17 13:52:22 +0300

    Create db.py

---
+++ app/lib/db.py
@@ -0,0 +1,27 @@
+import os
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_async_engine(
+    os.getenv('PG_URI_ASYNC'),
+    echo=False,
+    future=True,
+    pool_size=10,
+    max_overflow=5
+)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(
+        engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+    async with async_session() as session:
+        try:
+            yield session
+            await session.commit()
+        finally:
+            await session.rollback()
+            await session.close()
