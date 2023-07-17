133b4c3ccd2e166cabc9fe0408de3ef17f116526
savelmtr <56698459+savelmtr@users.noreply.github.com>
2023-07-17 13:53:18 +0300

    Create exceptions.py

---
+++ app/lib/exceptions.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException, status
+from typing import Any
+
+
+class _HTTPAny(HTTPException):
+    status_code = None
+    def __init__(
+        self,
+        detail: Any = None,
+        headers: dict[str, Any] | None = None,
+    ) -> None:
+        super().__init__(
+            status_code=self.status_code,
+            detail=detail,
+            headers=headers
+        )
+
+
+class HTTP500(_HTTPAny):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    
+
+class HTTP400(_HTTPAny):
+    status_code = status.HTTP_400_BAD_REQUEST
